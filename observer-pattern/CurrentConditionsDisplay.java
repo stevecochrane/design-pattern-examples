eaaef31e382a156044ca4ad9e4916b45ddac6d41
Steve Cochrane <steve@stevecochrane.com>
2018-02-17 17:37:24 -0600

    Refactored CurrentConditionsDisplay to use Observer and Observable.

--- observer-pattern/CurrentConditionsDisplay.java
+++ observer-pattern/CurrentConditionsDisplay.java
@@ -1,18 +1,25 @@
+import java.util.Observable;
+import java.util.Observer;
+
 public class CurrentConditionsDisplay implements Observer, DisplayElement {
 
+	Observable observable;
 	private float temperature;
 	private float humidity;
 	private Subject weatherData;
 
-	public CurrentConditionsDisplay(Subject weatherData) {
-		this.weatherData = weatherData;
-		weatherData.registerObserver(this);
+	public CurrentConditionsDisplay(Observable observable) {
+		this.observable = observable;
+		observable.addObserver(this);
 	}
 
-	public void update(float temperature, float humidity, float pressure) {
-		this.temperature = temperature;
-		this.humidity = humidity;
-		display();
+	public void update(Observable obs, Object arg) {
+		if (obs instanceof WeatherData) {
+			WeatherData weatherData = (WeatherData)obs;
+			this.temperature = weatherData.getTemperature();
+			this.humidity = weatherData.getHumidity();
+			display();
+		}
 	}
 
 	public void display() {
