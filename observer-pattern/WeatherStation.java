2d17c7f0e5ed130c8afb46de67c681ba3e7a054e
Steve Cochrane <steve@stevecochrane.com>
2018-02-17 17:19:19 -0600

    Commented out the two display types that don't exist yet.

--- observer-pattern/WeatherStation.java
+++ observer-pattern/WeatherStation.java
@@ -4,8 +4,8 @@ public class WeatherStation {
 		WeatherData weatherData = new WeatherData();
 
 		CurrentConditionsDisplay currentDisplay = new CurrentConditionsDisplay(weatherData);
-		StatitisticsDisplay statitisticsDisplay = new StatitisticsDisplay(weatherData);
-		ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+		// StatitisticsDisplay statitisticsDisplay = new StatitisticsDisplay(weatherData);
+		// ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
 
 		weatherData.setMeasurements(80, 65, 30.4f);
 		weatherData.setMeasurements(82, 70, 29.2f);
