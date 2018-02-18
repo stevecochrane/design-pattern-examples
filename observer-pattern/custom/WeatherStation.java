640556b6706937d49441e93667a4673be07ed0f3
Steve Cochrane <steve@stevecochrane.com>
2018-02-18 14:41:38 -0600

    Added a version of ForecastDisplay that doesn't use Observable.

--- observer-pattern/custom/WeatherStation.java
+++ observer-pattern/custom/WeatherStation.java
@@ -5,7 +5,7 @@ public class WeatherStation {
 
 		CurrentConditionsDisplay currentDisplay = new CurrentConditionsDisplay(weatherData);
 		// StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
-		// ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+		ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
 
 		weatherData.setMeasurements(80, 65, 30.4f);
 		weatherData.setMeasurements(82, 70, 29.2f);
