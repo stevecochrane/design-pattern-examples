10e39be13a320f643c2bed194b5a6fb1af0c62fb
Steve Cochrane <steve@stevecochrane.com>
2018-02-19 13:05:22 -0600

    Added a new abstract Beverage class.

---
+++ decorator/Beverage.java
@@ -0,0 +1,11 @@
+public abstract class Beverage {
+
+	String description = "Unknown beverage";
+
+	public String getDescription() {
+		return description;
+	}
+
+	public abstract double cost();
+
+}
