2dad4149c4cd7a4d1ba270901244605aaeb7dae9
Steve Cochrane <steve@stevecochrane.com>
2018-02-21 21:31:02 -0600

    Added more Beverage subclasses for Dark Roast and Decaf.

---
+++ decorator/DarkRoast.java
@@ -0,0 +1,11 @@
+public class DarkRoast extends Beverage {
+
+	public DarkRoast() {
+		description = "Dark Roast";
+	}
+
+	public double cost() {
+		return 0.99;
+	}
+
+}
---
+++ decorator/Decaf.java
@@ -0,0 +1,11 @@
+public class Decaf extends Beverage {
+
+	public Decaf() {
+		description = "Decaf";
+	}
+
+	public double cost() {
+		return 1.05;
+	}
+
+}
