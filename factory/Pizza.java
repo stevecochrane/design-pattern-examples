726e3262db7108e1984d3481a13a6bc05e3ee80b
Steve Cochrane <steve@stevecochrane.com>
2018-02-25 18:27:21 -0600

    Added a new abstract Pizza class.

---
+++ factory/Pizza.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public abstract class Pizza {
+
+	String name;
+	String dough;
+	String sauce;
+	ArrayList<String> toppings = new ArrayList<String>();
+
+	void prepare() {
+		System.out.println("Preparing " + name);
+		System.out.println("Tossing dough...");
+		System.out.println("Adding sauce...");
+		System.out.println("Adding toppings...");
+		for (String topping : toppings) {
+			System.out.println("  " + topping);
+		}
+	}
+
+	void bake() {
+		System.out.println("Baking for 25 minutes at 350 degrees...");
+	}
+
+	void cut() {
+		System.out.println("Cutting the pizza into diagonal slices...");
+	}
+
+	void box() {
+		System.out.println("Placing pizza in official PizzaStore box...");
+	}
+
+	public String getName() {
+		return name;
+	}
+
+}
