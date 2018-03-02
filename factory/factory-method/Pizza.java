76962a5ba49ba5f07c7e070e6024a411c3daeefc
Steve Cochrane <steve@stevecochrane.com>
2018-03-01 22:50:01 -0600

    Pizza now has an abstract prepare() method for more flexibility with preparing ingredients by region.

--- factory/factory-method/Pizza.java
+++ factory/factory-method/Pizza.java
@@ -3,19 +3,15 @@ import java.util.ArrayList;
 public abstract class Pizza {
 
 	String name;
-	String dough;
-	String sauce;
-	ArrayList<String> toppings = new ArrayList<String>();
 
-	void prepare() {
-		System.out.println("Preparing " + name);
-		System.out.println("Tossing dough...");
-		System.out.println("Adding sauce...");
-		System.out.println("Adding toppings...");
-		for (String topping : toppings) {
-			System.out.println("  " + topping);
-		}
-	}
+	Dough dough;
+	Sauce sauce;
+	Veggies veggies[];
+	Cheese cheese;
+	Pepperoni pepperoni;
+	Clams clams;
+
+	abstract void prepare();
 
 	void bake() {
 		System.out.println("Baking for 25 minutes at 350 degrees...");
@@ -29,8 +25,16 @@ public abstract class Pizza {
 		System.out.println("Placing pizza in official PizzaStore box...");
 	}
 
+	void setName(String name) {
+		this.name = name;
+	}
+
 	public String getName() {
 		return name;
+	}
+
+	public String toString() {
+		//	Code to print pizza goes here
 	}
 
 }
