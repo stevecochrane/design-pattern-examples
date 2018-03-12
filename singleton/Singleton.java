ae7f8f46e69af2b467c527e6515288f96e088e0e
Steve Cochrane <steve@stevecochrane.com>
2018-03-11 21:48:43 -0500

    Added a very simple example of the Singleton pattern.

---
+++ singleton/Singleton.java
@@ -0,0 +1,18 @@
+public class Singleton {
+
+	private static Singleton uniqueInstance;
+
+	//	other useful instance variables here
+
+	private Singleton() {}
+
+	public static Singleton getInstance() {
+		if (uniqueInstance == null) {
+			uniqueInstance = new Singleton();
+		}
+		return uniqueInstance;
+	}
+
+	//	other useful methods here
+
+}
