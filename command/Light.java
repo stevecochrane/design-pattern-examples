32cb7b9e2ae0d822a8465845c5f8a8431a5af9c9
Steve Cochrane <steve@stevecochrane.com>
2018-03-20 22:03:26 -0500

    Added a very simple Light class with on and off methods.

---
+++ command/Light.java
@@ -0,0 +1,13 @@
+public class Light {
+
+	public Light() {}
+
+	public void on() {
+		System.out.println("The light is now on.");
+	}
+
+	public void off() {
+		System.out.println("The light is now off.");
+	}
+
+}
