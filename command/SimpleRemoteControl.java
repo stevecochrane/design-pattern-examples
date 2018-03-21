58cfee3e076e314bd3444b7ccde2992efa608887
Steve Cochrane <steve@stevecochrane.com>
2018-03-20 21:58:52 -0500

    Added a new SimpleRemoteControl class where Command classes can be stored and executed.

---
+++ command/SimpleRemoteControl.java
@@ -0,0 +1,15 @@
+public class SimpleRemoteControl {
+
+	Command slot;
+
+	public SimpleRemoteControl() {}
+
+	public void setCommand(Command command) {
+		slot = command;
+	}
+
+	public void buttonWasPressed() {
+		slot.execute();
+	}
+
+}
