384751fa5c314167f52af1b51a0360741c018a74
Steve Cochrane <steve@stevecochrane.com>
2018-03-20 22:04:10 -0500

    Added a new RemoteControlTest class that assigns LightOnCommand to SimpleRemoteControl.

---
+++ command/RemoteControlTest.java
@@ -0,0 +1,12 @@
+public class RemoteControlTest {
+
+	public static void main(String[] args) {
+		SimpleRemoteControl remote = new SimpleRemoteControl();
+		Light light = new Light();
+		LightOnCommand lightOn = new LightOnCommand(light);
+
+		remote.setCommand(lightOn);
+		remote.buttonWasPressed();
+	}
+
+}
