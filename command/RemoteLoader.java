e5ac50e684b923f642490238dbaa023f9694acb6
Steve Cochrane <steve@stevecochrane.com>
2018-03-21 16:51:05 -0500

    Added a new RemoteLoader class for more in-depth testing, though it references some classes that don't exist yet.

---
+++ command/RemoteLoader.java
@@ -0,0 +1,43 @@
+public class RemoteLoader {
+
+	public static void main(String[] args) {
+		RemoteControl remoteControl = new RemoteControl();
+
+		Light livingRoomLight = new Light("Living Room");
+		Light kitchenLight = new Light("Kitchen");
+		CeilingFan ceilingFan = new CeilingFan("Living Room");
+		GarageDoor garageDoor = new GarageDoor("");
+		Stereo stereo = new Stereo("Living Room");
+
+		LightOnCommand livingRoomLightOn = new LightOnCommand(livingRoomLight);
+		LightOffCommand livingRoomLightOff = new LightOffCommand(livingRoomLight);
+		LightOnCommand kitchenLightOn = new LightOnCommand(kitchenRoomLight);
+		LightOffCommand kitchenLightOff = new LightOffCommand(kitchenRoomLight);
+
+		CeilingFanOnCommand ceilingFanOn = new CeilingFanOnCommand(ceilingFan);
+		CeilingFanOffCommand ceilingFanOff = new CeilingFanOffCommand(ceilingFan);
+
+		GarageDoorUpCommand garageDoorUp = new GarageDoorUpCommand(garageDoor);
+		GarageDoorDownCommand garageDoorDown = new GarageDoorDownCommand(garageDoor);
+
+		StereoOnWithCDCommand stereoOnWithCD = new StereoOnWithCDCommand(stereo);
+		StereoOffCommand stereoOff = new StereoOffCommand(stereo);
+
+		remoteControl.setCommand(0, livingRoomLightOn, livingRoomLightOff);
+		remoteControl.setCommand(1, kitchenLightOn, kitchenLightOff);
+		remoteControl.setCommand(2, ceilingFanOn, ceilingFanOff);
+		remoteControl.setCommand(3, stereoOnWithCD, stereoOff);
+
+		System.out.println(remoteControl);
+
+		remoteControl.onButtonWasPressed(0);
+		remoteControl.offButtonWasPressed(0);
+		remoteControl.onButtonWasPressed(1);
+		remoteControl.offButtonWasPressed(1);
+		remoteControl.onButtonWasPressed(2);
+		remoteControl.offButtonWasPressed(2);
+		remoteControl.onButtonWasPressed(3);
+		remoteControl.offButtonWasPressed(3);
+	}
+
+}
