6a8da05c6caf91828124ea949c44ad29422579b4
Steve Cochrane <steve@stevecochrane.com>
2018-03-20 22:07:42 -0500

    Added a simple GarageDoor class with five methods.

---
+++ command/GarageDoor.java
@@ -0,0 +1,25 @@
+public class GarageDoor {
+
+	public GarageDoor() {}
+
+	public void up() {
+		System.out.println("The garage door is now up.");
+	}
+
+	public void down() {
+		System.out.println("The garage door is now down.");
+	}
+
+	public void stop() {
+		System.out.println("The garage door has stopped.");
+	}
+
+	public void lightOn() {
+		System.out.println("The garage door's light has turned on.");
+	}
+
+	public void lightOff() {
+		System.out.println("The garage door's light has turned off.");
+	}
+
+}
