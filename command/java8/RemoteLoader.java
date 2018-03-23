9e5b693267a42fac2261ae979b6b5a1ca8dcfe4c
Steve Cochrane <steve@stevecochrane.com>
2018-03-22 21:28:15 -0500

    Added a new java8 RemoteLoader utilizing lambdas.

--- command/java8/RemoteLoader.java
+++ command/java8/RemoteLoader.java
@@ -1,49 +1,26 @@
 public class RemoteLoader {
 
 	public static void main(String[] args) {
-		RemoteControlWithUndo remoteControl = new RemoteControlWithUndo();
+		RemoteControl remoteControl = new RemoteControl();
 
 		Light livingRoomLight = new Light("Living Room");
 		Light kitchenLight = new Light("Kitchen");
 		CeilingFan ceilingFan = new CeilingFan("Living Room");
-		GarageDoor garageDoor = new GarageDoor("");
+		GarageDoor garageDoor = new GarageDoor("Main House");
 		Stereo stereo = new Stereo("Living Room");
-		TV tv = new TV("Living Room");
-		HotTub hotTub = new HotTub("Patio");
 
-		LightOnCommand livingRoomLightOn = new LightOnCommand(livingRoomLight);
-		LightOffCommand livingRoomLightOff = new LightOffCommand(livingRoomLight);
-		LightOnCommand kitchenLightOn = new LightOnCommand(kitchenLight);
-		LightOffCommand kitchenLightOff = new LightOffCommand(kitchenLight);
+		remoteControl.setCommand(0, livingRoomLight::on, livingRoomLight::off);
+		remoteControl.setCommand(1, kitchenLight::on, kitchenLight::off);
+		remoteControl.setCommand(2, ceilingFan::high, ceilingFan::off);
 
-		CeilingFanMediumCommand ceilingFanMedium = new CeilingFanMediumCommand(ceilingFan);
-		CeilingFanHighCommand ceilingFanHigh = new CeilingFanHighCommand(ceilingFan);
-		CeilingFanOffCommand ceilingFanOff = new CeilingFanOffCommand(ceilingFan);
+		Command stereoOnWithCD = () -> {
+			stereo.on();
+			stereo.setCd();
+			stereo.setVolume(11);
+		};
 
-		GarageDoorUpCommand garageDoorUp = new GarageDoorUpCommand(garageDoor);
-		GarageDoorDownCommand garageDoorDown = new GarageDoorDownCommand(garageDoor);
-
-		StereoOnWithCDCommand stereoOnWithCD = new StereoOnWithCDCommand(stereo);
-		StereoOffCommand stereoOff = new StereoOffCommand(stereo);
-
-		TVOnCommand tvOn = new TVOnCommand(tv);
-		TVOffCommand tvOff = new TVOffCommand(tv);
-
-		HotTubOnCommand hotTubOn = new HotTubOnCommand(hotTub);
-		HotTubOffCommand hotTubOff = new HotTubOffCommand(hotTub);
-
-		Command[] partyOn = { livingRoomLightOn, stereoOnWithCD, tvOn, hotTubOn };
-		Command[] partyOff = { livingRoomLightOff, stereoOff, tvOff, hotTubOff };
-
-		MacroCommand partyOnMacro = new MacroCommand(partyOn);
-		MacroCommand partyOffMacro = new MacroCommand(partyOff);
-
-		remoteControl.setCommand(0, livingRoomLightOn, livingRoomLightOff);
-		remoteControl.setCommand(1, kitchenLightOn, kitchenLightOff);
-		remoteControl.setCommand(2, ceilingFanMedium, ceilingFanOff);
-		remoteControl.setCommand(3, ceilingFanHigh, ceilingFanOff);
-		remoteControl.setCommand(4, stereoOnWithCD, stereoOff);
-		remoteControl.setCommand(5, partyOnMacro, partyOffMacro);
+		remoteControl.setCommand(3, stereoOnWithCD, stereo::off);
+		remoteControl.setCommand(4, garageDoor::up, garageDoor::down);
 
 		System.out.println(remoteControl);
 
@@ -51,24 +28,12 @@ public class RemoteLoader {
 		remoteControl.offButtonWasPressed(0);
 		remoteControl.onButtonWasPressed(1);
 		remoteControl.offButtonWasPressed(1);
-
 		remoteControl.onButtonWasPressed(2);
 		remoteControl.offButtonWasPressed(2);
-		System.out.println(remoteControl);
-		remoteControl.undoButtonWasPressed();
-
 		remoteControl.onButtonWasPressed(3);
-		remoteControl.undoButtonWasPressed();
-		System.out.println(remoteControl);
-
+		remoteControl.offButtonWasPressed(3);
 		remoteControl.onButtonWasPressed(4);
 		remoteControl.offButtonWasPressed(4);
-
-		System.out.println("--- Pushing Macro On. Party Mode, Engage ---");
-		remoteControl.onButtonWasPressed(5);
-		System.out.println("--- Pushing Macro Off. Party Successful ---");
-		remoteControl.offButtonWasPressed(5);
-
 	}
 
 }
