882e9000a131e76f51747d9565d179d8f0fd33cd
Steve Cochrane <steve@stevecochrane.com>
2018-03-28 21:39:51 -0500

    Fixed variable instantiation problems in HomeTheaterTestDrive.

--- facade/HomeTheaterTestDrive.java
+++ facade/HomeTheaterTestDrive.java
@@ -1,14 +1,15 @@
 public class HomeTheaterTestDrive {
 
 	public static void main(String[] args) {
-		Amplifier amp;
-		Tuner tuner;
-		DvdPlayer dvd;
-		CdPlayer cd;
-		Projector projector;
-		TheaterLights lights;
-		Screen screen;
-		PopcornPopper popper;
+		Tuner tuner = new Tuner();
+		CdPlayer cd = new CdPlayer();
+		Projector projector = new Projector();
+		TheaterLights lights = new TheaterLights();
+		Screen screen = new Screen();
+		PopcornPopper popper = new PopcornPopper();
+
+		DvdPlayer dvd = new DvdPlayer(projector);
+		Amplifier amp = new Amplifier(tuner, dvd, cd);
 
 		HomeTheaterFacade homeTheater =
 			new HomeTheaterFacade(amp, tuner, dvd, cd, projector, screen, lights, popper);
