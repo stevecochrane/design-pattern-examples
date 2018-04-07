a24854470dcb81408236fe224a5b46f21580e25d
Steve Cochrane <steve@stevecochrane.com>
2018-04-07 16:12:29 -0500

    Add new CaffeineBeverage abstract class

---
+++ template/CaffeineBeverage.java
@@ -0,0 +1,22 @@
+public abstract class CaffeineBeverage {
+
+	final void prepareRecipe() {
+		boilWater();
+		brew();
+		pourInCup();
+		addCondiments();
+	}
+
+	abstract void brew();
+
+	abstract void addCondiments();
+
+	void boilWater() {
+		System.out.println("Boiling water...");
+	}
+
+	void pourInCup() {
+		System.out.println("Pouring into cup...");
+	}
+
+}
