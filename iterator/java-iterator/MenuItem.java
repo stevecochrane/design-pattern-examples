b3410adfbe02411b713913a79cd4356ecfb052ba
Steve Cochrane <steve@stevecochrane.com>
2018-04-20 20:47:17 -0500

    Duplicate custom-iterator directory to make new java-iterator directory

---
+++ iterator/java-iterator/MenuItem.java
@@ -0,0 +1,31 @@
+public class MenuItem {
+
+	String name;
+	String description;
+	boolean vegetarian;
+	double price;
+
+	public MenuItem(String name, String description, boolean vegetarian, double price) {
+		this.name = name;
+		this.description = description;
+		this.vegetarian = vegetarian;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public boolean getVegetarian() {
+		return vegetarian;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+}
---
+++ iterator/java-iterator/PancakeHouseMenu.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+
+public class PancakeHouseMenu {
+
+	ArrayList<MenuItem> menuItems;
+
+	public PancakeHouseMenu() {
+		menuItems = new ArrayList<MenuItem>();
+
+		addItem("K&B's Pancake Breakfast",
+			"Pancakes with scrambled eggs and toast",
+			true,
+			2.99
+		);
+
+		addItem("Regular Pancake Breakfast",
+			"Pancakes with fried eggs and sausage",
+			false,
+			2.99
+		);
+
+		addItem("Blueberry Pancakes",
+			"Pancakes made with fresh blueberries",
+			true,
+			3.49
+		);
+
+		addItem("Waffles",
+			"Waffles with your choice of blueberries or strawberries",
+			true,
+			3.59
+		);
+	}
+
+	public void addItem(String name, String description, boolean vegetarian, double price) {
+		MenuItem menuItem = new MenuItem(name, description, vegetarian, price);
+		menuItems.add(menuItem);
+	}
+
+	public Iterator createIterator() {
+		return new PancakeHouseMenuIterator(menuItems);
+	}
+
+}
