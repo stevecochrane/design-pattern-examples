11cdda24aa1c8605a91c49fca25d24942065f219
Steve Cochrane <steve@stevecochrane.com>
2018-04-20 21:06:19 -0500

    Use Menu interface for Waitress to abstract away menu implementations further

--- iterator/java-iterator/Waitress.java
+++ iterator/java-iterator/Waitress.java
@@ -1,16 +1,18 @@
+import java.util.Iterator;
+
 public class Waitress {
 
-	PancakeHouseMenu pancakeHouseMenu;
-	DinerMenu dinerMenu;
+	Menu pancakeHouseMenu;
+	Menu dinerMenu;
 
-	public Waitress(PancakeHouseMenu pancakeHouseMenu, DinerMenu dinerMenu) {
+	public Waitress(Menu pancakeHouseMenu, Menu dinerMenu) {
 		this.pancakeHouseMenu = pancakeHouseMenu;
 		this.dinerMenu = dinerMenu;
 	}
 
 	public void printMenu() {
-		Iterator pancakeIterator = pancakeHouseMenu.createIterator();
-		Iterator dinerIterator = dinerMenu.createIterator();
+		Iterator<MenuItem> pancakeIterator = pancakeHouseMenu.createIterator();
+		Iterator<MenuItem> dinerIterator = dinerMenu.createIterator();
 
 		System.out.println("MENU\n----\nBREAKFAST");
 		printMenu(pancakeIterator);
