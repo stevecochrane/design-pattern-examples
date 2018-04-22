3576852e8d5129b7111f5fb21d4d26c4c577ab1e
Steve Cochrane <steve@stevecochrane.com>
2018-04-22 12:47:49 -0500

    Add new Composite pattern classes MenuComponent, MenuItem, and Menu

---
+++ composite/Menu.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Menu extends MenuComponent {
+
+	ArrayList<MenuComponent> menuComponents = new ArrayList<MenuComponent>();
+	String name;
+	String description;
+
+	public Menu(String name, String description) {
+		this.name = name;
+		this.description = description;
+	}
+
+	public void add(MenuComponent menuComponent) {
+		menuComponents.add(menuComponent);
+	}
+
+	public void remove(MenuComponent menuComponent) {
+		menuComponents.remove(menuComponent);
+	}
+
+	public MenuComponent getChild(int i) {
+		return menuComponents.get(i);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void print() {
+		System.out.print("\n" + getName());
+		System.out.println(", " + getDescription());
+		System.out.println("---------------------");
+
+		Iterator<MenuComponent> iterator = menuComponents.iterator();
+		while (iterator.hasNext()) {
+			MenuComponent menuComponent = iterator.next();
+			menuComponent.print();
+		}
+	}
+
+}
