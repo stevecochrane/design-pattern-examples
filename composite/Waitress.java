6bcb889e4cce3bd433b4ae2ea2b9ce305efdcc82
Steve Cochrane <steve@stevecochrane.com>
2018-04-22 14:20:33 -0500

    Add printVegetarianMenu method to Waitress

--- composite/Waitress.java
+++ composite/Waitress.java
@@ -1,3 +1,5 @@
+import java.util.Iterator;
+
 public class Waitress {
 
 	MenuComponent allMenus;
@@ -8,6 +10,20 @@ public class Waitress {
 
 	public void printMenu() {
 		allMenus.print();
+	}
+
+	public void printVegetarianMenu() {
+		Iterator<MenuComponent> iterator = allMenus.createIterator();
+
+		System.out.println("\nVEGETARIAN MENU\n-----");
+		while (iterator.hasNext()) {
+			MenuComponent menuComponent = iterator.next();
+			try {
+				if (menuComponent.isVegetarian()) {
+					menuComponent.print();
+				}
+			} catch (UnsupportedOperationException e) {}
+		}
 	}
 
 }
