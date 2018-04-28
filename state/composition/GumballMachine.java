50cf5a2b6a498dd7d1e11bf9658997dd8b36c6ba
Steve Cochrane <steve@stevecochrane.com>
2018-04-28 18:49:08 -0500

    Rewrite GumballMachine to use state classes

---
+++ state/composition/GumballMachine.java
@@ -0,0 +1,49 @@
+public class GumballMachine {
+
+	State soldOutState;
+	State noQuarterState;
+	State hasQuarterState;
+	State soldState;
+
+	State state;
+	int count = 0;
+
+	public GumballMachine(int numberGumballs) {
+		soldOutState = new SoldOutState(this);
+		noQuarterState = new NoQuarterState(this);
+		hasQuarterState = new HasQuarterState(this);
+		soldState = new SoldState(this);
+
+		this.count = numberGumballs;
+		if (numberGumballs > 0) {
+			state = noQuarterState;
+		} else {
+			state = soldOutState;
+		}
+	}
+
+	public void insertQuarter() {
+		state.insertQuarter();
+	}
+
+	public void ejectQuarter() {
+		state.ejectQuarter();
+	}
+
+	public void turnCrank() {
+		state.turnCrank();
+		state.dispense();
+	}
+
+	void setState(State state) {
+		this.state = state;
+	}
+
+	void releaseBall() {
+		System.out.println("A gumball comes rolling out of the slot...");
+		if (count != 0) {
+			count = count - 1;
+		}
+	}
+
+}
