a1e7bc3cb3eda5bafa9de9ed78a15f68bcf0783b
Steve Cochrane <steve@stevecochrane.com>
2018-04-28 18:44:36 -0500

    Add new 'composition' directory with new NoQuarterState class

---
+++ state/composition/NoQuarterState.java
@@ -0,0 +1,26 @@
+public class NoQuarterState implements State {
+
+	GumballMachine gumballMachine;
+
+	public NoQuarterState(GumballMachine gumballMachine) {
+		this.gumballMachine = gumballMachine;
+	}
+
+	public void insertQuarter() {
+		System.out.println("You inserted a quarter.");
+		gumballMachine.setState(gumballMachine.getHasNoQuarterState());
+	}
+
+	public void ejectQuarter() {
+		System.out.println("You haven't inserted a quarter.");
+	}
+
+	public void turnCrank() {
+		System.out.println("You turned, but there's no quarter.");
+	}
+
+	public void dispense() {
+		System.out.println("You need to pay first!");
+	}
+
+}
