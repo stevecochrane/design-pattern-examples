029bfe58b3bc2715965a641fa9b6b0654ed92c4e
Steve Cochrane <steve@stevecochrane.com>
2018-04-28 19:17:19 -0500

    Add feature to dispense bonus gumball for 1 out of 10 customers

--- state/composition/GumballMachine.java
+++ state/composition/GumballMachine.java
@@ -4,6 +4,7 @@ public class GumballMachine {
 	State noQuarterState;
 	State hasQuarterState;
 	State soldState;
+	State winnerState;
 
 	State state;
 	int count = 0;
@@ -13,6 +14,7 @@ public class GumballMachine {
 		noQuarterState = new NoQuarterState(this);
 		hasQuarterState = new HasQuarterState(this);
 		soldState = new SoldState(this);
+		winnerState = new WinnerState(this);
 
 		this.count = numberGumballs;
 		if (numberGumballs > 0) {
@@ -53,6 +55,10 @@ public class GumballMachine {
 
 	public State getSoldState() {
 		return soldState;
+	}
+
+	public State getWinnerState() {
+		return winnerState;
 	}
 
 	public int getCount() {
--- state/composition/HasQuarterState.java
+++ state/composition/HasQuarterState.java
@@ -1,5 +1,8 @@
+import java.util.Random;
+
 public class HasQuarterState implements State {
 
+	Random randomWinner = new Random(System.currentTimeMillis());
 	GumballMachine gumballMachine;
 
 	public HasQuarterState(GumballMachine gumballMachine) {
@@ -17,6 +20,12 @@ public class HasQuarterState implements State {
 
 	public void turnCrank() {
 		System.out.println("You turned...");
+		int winner = randomWinner.nextInt(10);
+		if ((winner == 0) && (gumballMachine.getCount() > 1)) {
+			gumballMachine.setState(gumballMachine.getWinnerState());
+		} else {
+			gumballMachine.setState(gumballMachine.getSoldState());
+		}
 	}
 
 	public void dispense() {
