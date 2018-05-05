7a364beb36ad3a15f989a2c6d03358169b2ae629
Steve Cochrane <steve@stevecochrane.com>
2018-05-05 14:21:23 -0500

    Add WinnerState like the other State implementations

---
+++ proxy/WinnerState.java
@@ -0,0 +1,39 @@
+public class WinnerState implements State {
+
+	private static final long serialVersionUID = 2L;
+
+	transient GumballMachine gumballMachine;
+
+	public WinnerState(GumballMachine gumballMachine) {
+		this.gumballMachine = gumballMachine;
+	}
+
+	public void insertQuarter() {
+		System.out.println("Please wait, we're already giving you a gumball.");
+	}
+
+	public void ejectQuarter() {
+		System.out.println("Sorry, you already turned the crank.");
+	}
+
+	public void turnCrank() {
+		System.out.println("Turning twice doesn't get you another gumball!");
+	}
+
+	public void dispense() {
+		gumballMachine.releaseBall();
+		if (gumballMachine.getCount() == 0) {
+			gumballMachine.setState(gumballMachine.getSoldOutState());
+		} else {
+			gumballMachine.releaseBall();
+			System.out.println("YOU'RE A WINNER! You get two gumballs for your quarter.");
+			if (gumballMachine.getCount() > 0) {
+				gumballMachine.setState(gumballMachine.getNoQuarterState());
+			} else {
+				System.out.println("Oops, out of gumballs!");
+				gumballMachine.setState(gumballMachine.getSoldOutState());
+			}
+		}
+	}
+
+}
