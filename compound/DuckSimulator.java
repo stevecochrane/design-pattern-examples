4c555f2581e8a28184451dbc7378010531dfcaef
Steve Cochrane <steve@stevecochrane.com>
2018-05-05 16:41:02 -0500

    Integrate Flock composites with DuckSimulator

--- compound/DuckSimulator.java
+++ compound/DuckSimulator.java
@@ -14,13 +14,34 @@ public class DuckSimulator {
 		Quackable rubberDuck = duckFactory.createRubberDuck();
 		Quackable gooseDuck = new GooseAdapter(new Goose());
 
-		System.out.println("\nDuck Simulator: With Abstract Factory");
+		System.out.println("\nDuck Simulator: With Abstract Factory and Composite - Flocks");
 
-		simulate(mallardDuck);
-		simulate(redheadDuck);
-		simulate(duckCall);
-		simulate(rubberDuck);
-		simulate(gooseDuck);
+		Flock flockOfDucks = new Flock();
+
+		flockOfDucks.add(mallardDuck);
+		flockOfDucks.add(duckCall);
+		flockOfDucks.add(rubberDuck);
+		flockOfDucks.add(gooseDuck);
+
+		Flock flockOfMallards = new Flock();
+
+		Quackable mallardOne = duckFactory.createMallardDuck();
+		Quackable mallardTwo = duckFactory.createMallardDuck();
+		Quackable mallardThree = duckFactory.createMallardDuck();
+		Quackable mallardFour = duckFactory.createMallardDuck();
+
+		flockOfMallards.add(mallardOne);
+		flockOfMallards.add(mallardTwo);
+		flockOfMallards.add(mallardThree);
+		flockOfMallards.add(mallardFour);
+
+		flockOfDucks.add(flockOfMallards);
+
+		System.out.println("\nDuckSimulator: Whole Flock Simulation");
+		simulate(flockOfDucks);
+
+		System.out.println("\nDuckSimulator: Mallard Flock Simulation");
+		simulate(flockOfMallards);
 
 		System.out.println("The ducks quacked " + QuackCounter.getQuacks() + " times");
 	}
