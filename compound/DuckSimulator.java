dc95579de7706ecef158e33012af7900b4e30fa8
Steve Cochrane <steve@stevecochrane.com>
2018-05-05 16:29:10 -0500

    Integrate AbstractDuckFactory with DuckSimulator

--- compound/DuckSimulator.java
+++ compound/DuckSimulator.java
@@ -2,17 +2,19 @@ public class DuckSimulator {
 
 	public static void main(String[] args) {
 		DuckSimulator simulator = new DuckSimulator();
-		simulator.simulate();
+		AbstractDuckFactory duckFactory = new CountingDuckFactory();
+
+		simulator.simulate(duckFactory);
 	}
 
-	void simulate() {
-		Quackable mallardDuck = new QuackCounter(new MallardDuck());
-		Quackable redheadDuck = new QuackCounter(new RedheadDuck());
-		Quackable duckCall = new QuackCounter(new DuckCall());
-		Quackable rubberDuck = new QuackCounter(new RubberDuck());
+	void simulate(AbstractDuckFactory duckFactory) {
+		Quackable mallardDuck = duckFactory.createMallardDuck();
+		Quackable redheadDuck = duckFactory.createRedheadDuck();
+		Quackable duckCall = duckFactory.createDuckCall();
+		Quackable rubberDuck = duckFactory.createRubberDuck();
 		Quackable gooseDuck = new GooseAdapter(new Goose());
 
-		System.out.println("\nDuck Simulator");
+		System.out.println("\nDuck Simulator: With Abstract Factory");
 
 		simulate(mallardDuck);
 		simulate(redheadDuck);
