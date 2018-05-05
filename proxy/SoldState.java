e9a554d1d868b11156c46d0e3cd4752670533655
Steve Cochrane <steve@stevecochrane.com>
2018-05-05 14:18:26 -0500

    Update State implementations to allow for transient GumballMachine

---
+++ proxy/SoldState.java
@@ -0,0 +1,33 @@
+public class SoldState implements State {
+
+	private static final long serialVersionUID = 2L;
+
+	transient GumballMachine gumballMachine;
+
+	public SoldState(GumballMachine gumballMachine) {
+		this.gumballMachine = gumballMachine;
+	}
+
+	public void insertQuarter() {
+		System.out.println("Please wait, we're already giving you a gumball.");
+	}
+
+	public void ejectQuarter() {
+		System.out.println("Sorry, you already turned the crank.");
+	}
+
+	public void turnCrank() {
+		System.out.println("Turning twice doesn't get you another gumball!");
+	}
+
+	public void dispense() {
+		gumballMachine.releaseBall();
+		if (gumballMachine.getCount() > 0) {
+			gumballMachine.setState(gumballMachine.getNoQuarterState());
+		} else {
+			System.out.println("Oops, just ran out of gumballs!");
+			gumballMachine.setState(gumballMachine.getSoldOutState());
+		}
+	}
+
+}
